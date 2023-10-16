f7db370bd4999b4034ec183da78743730225478e
reimarii <reimarii@oamk.fi>
2023-10-17 00:12:04 +0300

    XML example added

---
+++ xml_example.js
@@ -0,0 +1,34 @@
+const { default: axios } = require('axios');
+var xml2js = require('xml2js');
+
+getXml();
+
+//Promise version
+async function getXml(){
+    try {
+        const result = await axios.get('https://www.finnkino.fi/xml/NewsCategories/');
+
+        const jsResult = await xml2js.parseStringPromise(result.data);
+        
+        jsResult.NewsCategories.NewsArticleCategory.forEach(e => {
+            console.log(e.Name[0]);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Callback version
+// axios.get('https://www.finnkino.fi/xml/NewsCategories/')
+//     .then( res=> {
+//         parseString(res.data, (err, result) => {
+//             result.NewsCategories.NewsArticleCategory.forEach(e => {
+//                 console.log(e.Name[0]);
+//             });
+//         })
+//     });
+
+
+
